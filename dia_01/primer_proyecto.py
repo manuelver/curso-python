62d6dfc3939583ef9874775ab46383ef8085ff8a
Manuel Vergara <manuel@vergaracarmona.es>
2023-04-12 21:53:28 +0200

    ADD dia 01

---
+++ dia_01/primer_proyecto.py
@@ -0,0 +1,26 @@
+""" Primeros ejercicios """
+
+print("Hola mundo")
+
+print(100 + 50)
+
+print("\nHOLA" + " " + 'MUNDO')
+
+print("\nMe llamo \"Manuel\"")
+
+print("Primera linea\nSegunda linea")
+
+print("\ttabulemos")
+
+print("This isn't a number")
+print('This isn\'t a number')
+print("Este signo \"\\\" es una barra invertida")
+
+print("\n\nHola ")
+print("Tu nombre es " + input("Introduce tu nombre: ")
+      + " y tu apellido es "
+      + input("Introduce tu apellido: "))
+
+print(input("¿Qué estás estudiando? "))
+
+print(input("Escribe tu nombre: ") + " " + input("Escribe tu apellido: "))
---
+++ dia_01/programa01.py
@@ -0,0 +1,4 @@
+""" programa 01 del curso """
+print("La cerveza se llama\n"
+      + "\"" + input("¿Cuál es tu mascota favorita? ") + " " + input("¿Cuál es tu color favorito? ")
+      + "\"\n¡Felicidades por tu espumosa!")
