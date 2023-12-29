159d9490a45515bb0ba0f28e834cabd45095a1d5
Manuel Vergara <manuel@vergaracarmona.es>
2023-12-29 05:35:02 +0100

    Update Python Ofensivo

--- python-ofensivo/00_ejercicios/07_modulos01.py
+++ python-ofensivo/00_ejercicios/07_modulos01.py
@@ -1,12 +1,16 @@
 """
 Se puede ver con dir() las funciones que tiene un módulo
 
-Con builtin_module_names se puede ver los módulos que vienen por defecto en python
+Con builtin_module_names se puede ver los módulos que vienen por defecto en 
+python
 
 Por supuesto, se pueden ver todos los módulos descargados con pip con:
 pip freeze
 
-Se puede ver la ubicación de un módulo externo con __file__
+Se puede ver la ubicación de un módulo externo con __file__ 
+
+También se puede buscar la ubicación de los módulos con el path de python con 
+sys.path
 
 """
 
@@ -20,6 +24,8 @@ print()
 print(sys.builtin_module_names)
 print()
 print(hashlib.__file__)
+print()
+print(sys.path)
 print()
 
 # Ejemplo de uso hashlib
